2617a39505b5a95773db4ca9411587b8f66ebfdb
devbriguy <briansinnicke@gmail.com>
2017-01-08 23:14:25 -0500

    Update demo and screenshot; Minor changes to Main example

--- example/Main.js
+++ example/Main.js
@@ -25,19 +25,15 @@ export default class Main extends Component {
       <ScrollView contentContainerStyle={styles.container}>
         <Text style={[styles.text, {fontSize: 22}]}>{'Simple Stepper'}</Text>
         <View style={{flexDirection: 'column', alignItems: 'center'}}>
-
           <Text style={[styles.text, {fontSize: 32, color: 'blue'}]}>{this.state.blue}</Text>
           <SimpleStepper valueChanged={this.valueChangedBlue} />
           <Text style={styles.text}> {'\n'}{'min 0, max 10, initialValue is 0 and stepValue is 1'}{'\n'} </Text>
-
           <Text style={[styles.text, {fontSize: 32, color: 'peru'}]}>{this.state.peru}</Text>
-          <SimpleStepper valueChanged={this.valueChangedPeru} tintColor={'peru'} maximumValue={25} initialValue={.99} stepValue={.99}/>
-          <Text style={styles.text}> {'\n'}{'min is 0, max is 25, initialValue is .99 and stepValue is .99'}{'\n'} </Text>
-
+          <SimpleStepper valueChanged={this.valueChangedPeru} tintColor={'peru'} maximumValue={15} initialValue={.99} stepValue={.99}/>
+          <Text style={styles.text}> {'\n'}{'min is 0, max is 15, initialValue is .99 and stepValue is .99'}{'\n'} </Text>
           <Text style={[styles.text, {fontSize: 32, color: '#cc3232'}]}>{this.state.red}</Text>
           <SimpleStepper valueChanged={this.valueChangedRed} tintColor={'#cc3232'} minimumValue={-100} maximumValue={100} initialValue={50} stepValue={25} />
           <Text style={styles.text}> {'\n'}{'min is -100, max is 100, initialValue is 50 and stepValue is 25'}{'\n'} </Text>
-
         </View>
       </ScrollView>
     );

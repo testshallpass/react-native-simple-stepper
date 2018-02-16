88274baa4aac13c11f72afbd27dd4547d5d9b5d5
Brian Sinnicke <briansinnicke@gmail.com>
2018-02-15 21:50:30 -0500

    add code coverage; add more tests

--- __tests__/SimpleStepper-test.js
+++ __tests__/SimpleStepper-test.js
@@ -1,11 +1,14 @@
-import {Image} from 'react-native';
+import { Image } from 'react-native';
 import React from 'react';
 import SimpleStepper from '../SimpleStepper';
 import renderer from 'react-test-renderer';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+const reactNativeLogo = 'https://facebook.github.io/react-native/docs/assets/favicon.png';
 
 test('renders correctly', () => {
-  const tree = renderer.create(<SimpleStepper />).toJSON();
+  const wrapper = shallow(<SimpleStepper />);
+  const tree = toJson(wrapper);
   expect(tree).toMatchSnapshot();
 });
 test('test incrementAction', () => {
@@ -45,22 +48,20 @@ test('validate max value to be true', () => {
   expect(wrapper.state().hasReachedMax).toBe(true);
 });
 test('tintColor to be blue', () => {
-  const wrapper = shallow(<SimpleStepper />);
+  const wrapper = shallow(<SimpleStepper tintColor={'blue'} />);
   const tintStyle = wrapper.instance().tintStyle(true);
-  expect(tintStyle).toEqual({tintColor: 'blue'});
+  expect(tintStyle).toEqual({ tintColor: 'blue' });
 });
 test('tintColor to be null', () => {
-  const wrapper = shallow(<SimpleStepper />);
+  const wrapper = shallow(<SimpleStepper tintColor={'blue'} />);
   const tintStyle = wrapper.instance().tintStyle(false);
   expect(tintStyle).toBeNull();
 });
 test('increment type imageSrc to be uri object', () => {
   const wrapper = shallow(<SimpleStepper />);
-  const imageSrc = wrapper
-    .instance()
-    .imageSrc('https://facebook.github.io/react/img/logo_og.png', 'increment');
+  const imageSrc = wrapper.instance().imageSrc(reactNativeLogo, 'increment');
   expect(imageSrc).toEqual({
-    uri: 'https://facebook.github.io/react/img/logo_og.png',
+    uri: reactNativeLogo,
   });
 });
 test('increment type imageSrc to be local asset number', () => {
@@ -88,27 +89,118 @@ test('unknown type imageSrc to be empty string', () => {
   const imageSrc = wrapper.instance().imageSrc('', 'unknown');
   expect(imageSrc).toEqual('');
 });
+test('unknown type imageSrc to be {uri: \' \'}', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  const imageSrc = wrapper.instance().imageSrc(' ', 'unknown');
+  expect(imageSrc).toEqual({uri: ' '});
+});
+test('imageStyle to be with non-empty src string', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  const imageStyle = wrapper.instance().imageStyle(' ', 36, 36);
+  expect(imageStyle).toEqual({ width: 36, height: 36 });
+});
 test('imageStyle to be null', () => {
   const wrapper = shallow(<SimpleStepper />);
   const imageStyle = wrapper.instance().imageStyle(null, 36, 36);
   expect(imageStyle).toBeNull();
 });
+test('imageStyle to be null with empty string', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  const imageStyle = wrapper.instance().imageStyle('', 36, 36);
+  expect(imageStyle).toBeNull();
+});
 test('imageStyle to be style object', () => {
   const wrapper = shallow(<SimpleStepper />);
-  const imageStyle = wrapper
-    .instance()
-    .imageStyle('https://facebook.github.io/react/img/logo_og.png', 69, 69);
-  expect(imageStyle).toEqual({width: 69, height: 69});
+  const imageStyle = wrapper.instance().imageStyle(reactNativeLogo, 69, 69);
+  expect(imageStyle).toEqual({ width: 69, height: 69 });
 });
 test('renderImage to be Image', () => {
   const wrapper = shallow(<SimpleStepper />);
   const renderImage = wrapper.instance().renderImage('eeeee', {}, {}, 1, 1);
-  expect(renderImage).toEqual(
-    <Image source={1} style={[{}, {}, {opacity: 1}]} />,
-  );
+  expect(renderImage).toEqual(<Image source={1} style={[{}, {}, { opacity: 1 }]} />);
 });
 test('renderImage to be Mock Function', () => {
   const wrapper = shallow(<SimpleStepper />);
   const renderImage = wrapper.instance().renderImage(jest.fn, {}, {}, 1, 1);
   expect(jest.isMockFunction(renderImage)).toEqual(true);
+});
+test('validateValue to warn with step equal to 0', () => {
+  const wrapper = shallow(<SimpleStepper valueChanged={jest.fn()} />);
+  const validateValue = wrapper.instance().validateValue(10, 1, 11, false, 0, false);
+  expect(wrapper.instance().state.stepValue).toEqual(0);
+});
+test('stepValue equal to -1 with wraps', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  const validateValue = wrapper.instance().validateValue(0, -5, 5, false, -1, true);
+  expect(wrapper.instance().state.stepValue).toEqual(-1);
+});
+test('stepValue equal to -1 without wrap', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  const validateValue = wrapper.instance().validateValue(6, -5, 5, false, -1, false);
+  expect(wrapper.instance().state.stepValue).toEqual(-1);
+});
+test('test initialValue and stepValue prop change', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  wrapper.setProps({
+    initialValue: 1,
+    stepValue: 2,
+  });
+  expect(wrapper.instance()).toBeDefined();
+});
+test('test disabled prop change', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  wrapper.setProps({
+    disabled: true,
+  });
+  expect(wrapper.instance()).toBeDefined();
+});
+test('test min and max prop change', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  wrapper.setProps({
+    minimumValue: 5,
+    maximumValue: 30,
+  });
+  expect(wrapper.instance()).toBeDefined();
+});
+test('test min and max prop change less and greater', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  wrapper.setProps({
+    minimumValue: 11,
+    maximumValue: -1,
+  });
+  expect(wrapper.instance()).toBeDefined();
+});
+test('test max prop chang less than min', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  wrapper.setProps({
+    maximumValue: -1,
+  });
+  expect(wrapper.instance()).toBeDefined();
+});
+test('test min prop change greater than max', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  wrapper.setProps({
+    minimumValue: 11,
+  });
+  expect(wrapper.instance()).toBeDefined();
+});
+test('test initialValue, disabled, min and max prop change', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  wrapper.setProps({
+    initialValue: 0,
+    disabled: false,
+    minimumValue: 0,
+    maximumValue: 10,
+  });
+  expect(wrapper.instance()).toBeDefined();
+});
+test('test initialValue, disabled, min and max prop change with max less than min', () => {
+  const wrapper = shallow(<SimpleStepper />);
+  wrapper.setProps({
+    initialValue: 0,
+    disabled: false,
+    minimumValue: 9,
+    maximumValue: -1,
+  });
+  expect(wrapper.instance()).toBeDefined();
 });
